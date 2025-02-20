3130f79aa57857b56bea374e8b2f70cc76bdc5b4
guanzhy666 <1812771305@qq.com>
2025-02-20 14:00:06 +0800

    version 0.0.1

---
+++ planning_main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "process.h"
+using std::cout,std::endl;
+
+int main()
+{
+    cout<<"planning start"<<endl;
+    Process my_process;
+    my_process.processInfo();
+    cout<<"planing end"<<endl;
+    return 0;
+}
